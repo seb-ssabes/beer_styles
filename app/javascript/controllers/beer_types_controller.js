610b25d4dc3a78abe5f13ec300a59ecd0c9bdf05
Sebastian Olivares <olivares.heredia@gmail.com>
2025-02-13 13:41:02 +0100

    beer type animation transitions running dinamically

--- app/javascript/controllers/beer_types_controller.js
+++ app/javascript/controllers/beer_types_controller.js
@@ -1,12 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-  connect() {
-    console.log("Hola")
-  }
+  static targets = ["beerText"]
 
   beerTypeHover(e) {
-    console.log(e.target.dataset.beerType);
+    const type = e.target.dataset.beerType;
+
+    this.beerTextTarget.classList.remove("animate-slide-up");
+    void this.beerTextTarget.offsetWidth;
+
+    this.beerTextTarget.innerText = type;
+
+    this.beerTextTarget.classList.add("animate-slide-up");
+  }
+
+  beerHoverLeave() {
+    console.log("leaving")
+    this.beerTextTarget.innerText = "any Beer Type";
+
+    this.beerTextTarget.classList.remove("animate-slide-up");
+    void this.beerTextTarget.offsetWidth;
+
+    this.beerTextTarget.classList.add("animate-slide-up");
   }
 }
