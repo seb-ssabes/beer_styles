c6b5ae96394175c25fa698f7425e8dd7ef764bc1
Sebastian Olivares <olivares.heredia@gmail.com>
2025-02-22 17:07:59 +0100

    ajusted css for icon border and added delay on span tag text change

--- app/javascript/controllers/beer_types_controller.js
+++ app/javascript/controllers/beer_types_controller.js
@@ -4,14 +4,17 @@ export default class extends Controller {
   static targets = ["beerText"]
 
   connect() {
-    console.log("Hola from Types")
+    console.log("Hola from Types");
+    this.hoverTimeout = null;
   }
 
   beerTypeHover(e) {
     const type = e.currentTarget.dataset.beerTypeName;
     const text = this.beerTextTarget.classList;
-    const icons = document.querySelectorAll(".beer-icon")
-    const hoveredIcon = e.currentTarget.querySelector(".beer-icon")
+    const icons = document.querySelectorAll(".beer-icon");
+    const hoveredIcon = e.currentTarget.querySelector(".beer-icon");
+
+    clearTimeout(this.hoverTimeout);
 
     text.remove("animate-slide-up");
     void this.beerTextTarget.offsetWidth;
@@ -23,22 +26,30 @@ export default class extends Controller {
     icons.forEach(icon => {
       if (icon !== hoveredIcon) {
         icon.classList.add("opacity-40");
+        icon.classList.remove("dark-gray");
+      } else {
+        icon.classList.add("dark-gray");
       }
     })
+
+    e.currentTarget.classList.add("hovered-border");
   }
 
   beerHoverLeave() {
     const text = this.beerTextTarget.classList;
     const icons = document.querySelectorAll(".beer-icon");
 
+    this.hoverTimeout = setTimeout(() => {
+      this.beerTextTarget.innerText = "any Beer Type";
 
-    this.beerTextTarget.innerText = "any Beer Type";
 
-    text.remove("animate-slide-up");
-    void this.beerTextTarget.offsetWidth;
+      text.remove("animate-slide-up");
+      void this.beerTextTarget.offsetWidth;
 
-    text.add("animate-slide-up");
+      text.add("animate-slide-up");
 
-    icons.forEach(icon => icon.classList.remove("opacity-40"));
+      e.currentTarget.classList.remove("hovered-border");
+    }, 2000);
+    icons.forEach(icon => icon.classList.remove("opacity-40", "dark-gray"));
   }
 }
