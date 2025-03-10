3ad92611f1195df30d2c88655d55b3fb761d32a5
Sebastian Olivares <olivares.heredia@gmail.com>
2025-03-10 12:04:15 +0100

    added dinamic flag helper method for beer_style.origin

--- app/helpers/beer_styles_helper.rb
+++ app/helpers/beer_styles_helper.rb
@@ -1,2 +1,23 @@
 module BeerStylesHelper
+  def country_flag(country)
+    mapping = {
+      "USA" => "🇺🇸",
+      "United States" => "🇺🇸",
+      "United Kingdom" => "🇬🇧",
+      "England" => "🏴󠁧󠁢󠁥󠁮󠁧󠁿",
+      "Scotland" => "🏴󠁧󠁢󠁳󠁣󠁴󠁿",
+      "Ireland" => "🇮🇪",
+      "Germany" => "🇩🇪",
+      "Belgium" => "🇧🇪",
+      "France" => "🇫🇷",
+      "Czech Republic" => "🇨🇿",
+      "Poland" => "🇵🇱",
+      "Caribbean" => "Caribbean",
+      "Africa, Caribbean" => "Africa, Caribbean",
+      "Global" => "🌐",
+      "Various" => "🌐"
+    }
+
+    mapping[country] || "un"
+  end
 end
