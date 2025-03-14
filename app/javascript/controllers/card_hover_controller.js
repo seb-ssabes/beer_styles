dbf9880944ba62ece9a63bc1336ee90d1aaafc78
Sebastian Olivares <olivares.heredia@gmail.com>
2025-03-14 12:44:50 +0100

    fixed pop up window position and hovering behaviour

--- app/javascript/controllers/card_hover_controller.js
+++ app/javascript/controllers/card_hover_controller.js
@@ -4,26 +4,34 @@ export default class extends Controller {
   static targets = ["ibuExplainer", "srmExplainer"]
 
   connect() {
-    console.log("hola from Card Hover")
+    console.log("Hola from Card Hover")
   }
 
   ibuHover() {
-    this.ibuExplainerTarget.classList.remove("opacity-0", "scale-95");
-    this.ibuExplainerTarget.classList.add("opacity-100", "scale-100");
+    this.ibuExplainerTarget.classList.remove("opacity-0", "scale-95", "pointer-events-none");
+    this.ibuExplainerTarget.classList.add("opacity-100", "scale-100", "pointer-events-auto");
   }
 
   ibuExit() {
-    this.ibuExplainerTarget.classList.add("opacity-0", "scale-95");
-    this.ibuExplainerTarget.classList.remove("opacity-100", "scale-100");
+    this.hideTimeout = setTimeout(() => {
+      this.ibuExplainerTarget.classList.add("opacity-0", "scale-95", "pointer-events-none");
+      this.ibuExplainerTarget.classList.remove("opacity-100", "scale-100", "pointer-events-auto");
+    }, 200);
   }
 
   srmHover() {
-    this.srmExplainerTarget.classList.remove("opacity-0", "scale-95");
-    this.srmExplainerTarget.classList.add("opacity-100", "scale-100");
+    this.srmExplainerTarget.classList.remove("opacity-0", "scale-95", "pointer-events-none");
+    this.srmExplainerTarget.classList.add("opacity-100", "scale-100", "pointer-events-auto");
   }
 
   srmExit() {
-    this.srmExplainerTarget.classList.add("opacity-0", "scale-95");
-    this.srmExplainerTarget.classList.remove("opacity-100", "scale-100");
+    this.hideTimeout = setTimeout(() => {
+      this.srmExplainerTarget.classList.add("opacity-0", "scale-95", "pointer-events-none");
+      this.srmExplainerTarget.classList.remove("opacity-100", "scale-100", "pointer-events-auto");
+    }, 200);
+  }
+
+  keepOpen() {
+    clearTimeout(this.hideTimeout);
   }
 }
